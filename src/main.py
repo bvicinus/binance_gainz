1e0fb7cec621c6b7c730290f66670d2539324b7a
Brandon Vicinus <brandonvicinus@gmail.com>
2021-02-21 13:33:50 -0500

    store historical data

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 import argparse 
 import json
+import os
+import pathlib
 from binance.client import Client
+from datetime import datetime
 # local imports 
 from settings import config
 import calculate
@@ -25,6 +28,39 @@ def get_symbol_with_fiat(symbol: str) -> str:
     fiat_with_symbol = f'{symbol}{FIAT}'
     return fiat_with_symbol
 
+def store_historical_data(fiat_symbol: str, 
+                          quantity: float, 
+                          current_price: float,
+                          equity: float,
+                          gainz: float) -> None:
+    data = {
+        'fiat_symbol':fiat_symbol,
+        'quantity': quantity,
+        'current_price': current_price,
+        'equity': equity,
+        'gainz': gainz,
+        'timestamp': datetime.now().replace(microsecond=0).isoformat(),
+        'timestamp_utc': datetime.utcnow().replace(microsecond=0).isoformat(),
+    }
+
+    if not os.path.exists('data'):
+        os.mkdir('data')
+    file_path = os.path.join('data', f'{fiat_symbol}_history.json')
+    if not os.path.exists(file_path):
+        pathlib.Path(file_path).touch()
+    
+    with open(file_path, 'a') as f:
+        f.write(f'{json.dumps(data)},\n')
+
+def get_orders(fiat_symbol: str):
+    orders = client.get_all_orders(symbol=fiat_symbol)
+    if saveOrdersFile:
+        with open(os.path.join('data', 'orders.json'), 'w') as f:
+            f.write(json.dumps(orders, indent=2, separators=(',', ': ')))
+    print('Finished gathering all orders')
+    return orders
+        
+
 def main(symbol: str):
     status = client.get_system_status()
     if status.get('status') != 0:
@@ -37,11 +73,7 @@ def main(symbol: str):
     crypto_free = next((b.get('free') for b in balances if b.get('asset') == symbol), None)
     print(f'How much {symbol} owned right now: [{crypto_free}]')
 
-    orders = client.get_all_orders(symbol=fiat_symbol)
-    if saveOrdersFile:
-        with open('orders.json', 'w') as f:
-            f.write(json.dumps(orders, indent=2, separators=(',', ': ')))
-    print('Finished gathering all orders')
+    orders = get_orders(fiat_symbol=fiat_symbol)
 
     crypto_price = cmc.get_crypto_price(ticker=symbol)
     print(f'Current {fiat_symbol} price: [${crypto_price}]')
@@ -51,6 +83,12 @@ def main(symbol: str):
 
     gainz = calculate.unrealized_gains(order_list=orders, current_price=crypto_price, ticker=fiat_symbol)
     print(f'these are your {fiat_symbol} gainz [${gainz}]')
+    
+    store_historical_data(fiat_symbol=fiat_symbol,
+                          quantity=crypto_free,
+                          current_price=crypto_price,
+                          equity=equity,
+                          gainz=gainz)
 
 
 if __name__ == '__main__':
